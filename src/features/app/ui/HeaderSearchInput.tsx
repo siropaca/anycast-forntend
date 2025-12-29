86f83f5dfd163fcadb6065f0ffa2aaf86b4e08e3
siropaca <ryhshr3@gmail.com>
2025-12-29 22:03:35 +0900

    feat: HeaderSearchInput の入力変更で URL を更新

--- src/features/app/ui/HeaderSearchInput.tsx
+++ src/features/app/ui/HeaderSearchInput.tsx
@@ -1,14 +1,23 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { Pages } from '@/libs/pages';
 
 // TODO: 仮コンポーネント
-export function SearchInput() {
+export function HeaderSearchInput() {
   const router = useRouter();
 
+  const [value, setValue] = useState('');
+
   const handleFocus = () => {
-    router.push(Pages.explore.path());
+    router.push(Pages.explore.path({ q: value || undefined }));
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    router.push(Pages.explore.path({ q: newValue || undefined }));
   };
 
   return (
@@ -16,7 +25,9 @@ export function SearchInput() {
       type="text"
       placeholder="検索"
       className="border"
+      value={value}
       onFocus={handleFocus}
+      onChange={handleChange}
     />
   );
 }
